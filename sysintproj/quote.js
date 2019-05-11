f79fdfe0a284f5e1670c1450ef450443c686cfb3
Vidar Franzén <vexxart@gmail.com>
2019-05-11 13:39:35 +0200

    added features to the sysintproj

--- sysintproj/quote.js
+++ sysintproj/quote.js
@@ -1,15 +1,10 @@
 var qOfTday = document.getElementById("qOfTday");
 const quoteUrl = "https://quotes.rest/qod.json";
 
-function getQuote()
-{
     fetch(quoteUrl)
         .then(quoteResp => quoteResp.json())
             .then(quoteData =>
             {
-                console.log(quoteData.contents.quotes[0].quote);
-                qOfTday.innerHTML = `${quoteData.contents.quotes[0].quote}`;
+
+                qOfTday.innerHTML = `"${quoteData.contents.quotes[0].quote}"`;
             })
-
-
-}
